3d35654fc8a884e25ad4f5a2f7761562373e812f
dimshik100 <dimshik100@gmail.com>
2019-03-19 00:23:20 +0200

    Website placeholder animation

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <place-holder/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import PlaceHolder from '@/components/PlaceHolder.vue';
+
+@Component({
+  components: {
+    PlaceHolder,
+  },
+})
+export default class Home extends Vue {}
+</script>
